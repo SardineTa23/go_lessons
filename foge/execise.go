0ccda8765395890034443116a750e549929ad8ef
SardineTa23 <wonderland.7878@gmail.com>
2021-12-24 00:18:47 +0900

    solve exercise

---
+++ foge/execise.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	f := 1.11
+	i := int(f)
+	fmt.Println(i)
+
+	s := []int{1, 2, 5, 6, 3, 1}
+	fmt.Println(s[2:4])
+
+	m := map[string]int{"Mike": 20, "Nancy": 24, "Messi": 30}
+	fmt.Printf("%T %v", m, m)
+}
