64297bc27e5e615f609c6fa6bde16def6527e627
SardineTa23 <wonderland.7878@gmail.com>
2021-12-26 00:01:48 +0900

    solve execise

--- statements/execise.go
+++ statements/execise.go
@@ -18,4 +18,19 @@ func main() {
 	}
 
 	fmt.Println(min)
+
+	m := map[string]int{
+		"apple":  200,
+		"banana": 300,
+		"grapes": 150,
+		"orange": 80,
+		"papaya": 500,
+		"kiwi":   90,
+	}
+
+	sum := 0
+	for _, v := range m {
+		sum += v
+	}
+	fmt.Println(sum)
 }
